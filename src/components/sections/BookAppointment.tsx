4208b44f2e37924d5e6e68cf789affbf70cb2f45
TashaRolland92 <tasharolland@hotmail.co.uk>
2026-06-19 17:48:18 +0100

    button improvements

--- src/components/sections/BookAppointment.tsx
+++ src/components/sections/BookAppointment.tsx
@@ -10,10 +10,10 @@ const BookAppointment = () => {
                 fill
                 className="object-cover object-center"
             />
-            <div className="relative z-10 container mx-auto py-10 px-7">
-                <h3 className="uppercase playfair text-2xl md:text-xl relative">Book Appointment</h3>
-                <h1 className="playfair-italic-700 text-4xl leading-snug md:text-6xl md:leading-14 mb-6">Make a reservation for unforgettable beauty.</h1>
-                <p className="playfair text-lg max-w-96 mx-auto mb-6">Trust your beauty journey to our accomplished team, bringing you the techniques and styles seen on the world's stage. Contact us today.</p>
+            <div className="relative z-10 container mx-auto py-10 px-8">
+                <h3 className="uppercase playfair text-2xl relative">Book Appointment</h3>
+                <h1 className="playfair-italic-700 text-4xl leading-snug md:text-6xl md:leading-14 mt-6 mb-6">Make a reservation for unforgettable beauty.</h1>
+                <p className="playfair text-lg max-w-md mx-auto mb-6">Trust your beauty journey to our accomplished team, bringing you the techniques and styles seen on the world's stage. Contact us today.</p>
                 <Button to="/booking">Book Online</Button>
             </div>
         </section>
